3142891cbc0746bec21e519376b9ae4ab1de09c9
sklavoug <93801496+sklavoug@users.noreply.github.com>
2024-09-23 20:29:28 +1000

    second push

--- hpt.py
+++ hpt.py
@@ -1,3 +1,13 @@
 # Horus Heresy Pretrained Transformer
 with open('all_text.txt', 'r', encoding='utf-8') as f:
     text = f.read()
+
+print(f'Number of characters in dataset: {len(text)}')
+
+# Unique characters
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+print(''.join(chars))
+print(vocab_size)
+
+# Tokenise
